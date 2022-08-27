9019ca9f2cfdd202a93b1e004897285e07256077
techchrism <chris@techchrism.me>
2022-08-27 12:04:53 -0700

    add project image support

--- src/projects.ts
+++ src/projects.ts
@@ -1,14 +1,17 @@
+import ValorantAPIDocsImage from "./assets/valorant-api-docs.png";
+
 export interface Project {
     repository: string
     name: string
     description: string
-    picture?: string
+    picture?: ImageMetadata
 }
 
 export const projects: Project[] = [
     {
         repository: 'https://github.com/techchrism/valorant-api-docs/tree/trunk/docs',
         name: 'Valorant API Docs',
-        description: 'Documentation for the Valorant API'
+        description: 'Documentation for the Valorant API',
+        picture: ValorantAPIDocsImage
     }
 ]
