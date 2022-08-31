e141ef5a736a064f287a170dbd67cb6e24eb1c07
techchrism <chris@techchrism.me>
2022-08-30 20:23:12 -0700

    add dark mode support

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'white-transparent': 'rgba(255, 255, 255, 0.8)',
+				'black-transparent': 'rgba(0, 0, 0, 0.8)',
+			}
+		},
 	},
 	plugins: [],
 }
